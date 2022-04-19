1cd177be251bd3b3b969ac785de4b2f6209502eb
Valentin <xerrox@yandex.ru>
2022-04-19 12:06:32 +0300

    Added feature user looks at other user page scenario test

---
+++ spec/features/user_looks_other_user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+require 'support/my_spec_helper'
+
+RSpec.feature 'USER looks at other user page', type: :feature do
+  let(:vasya) { create(:user, name: 'Vasya', balance: 0) }
+  let(:masha) { create(:user, name: 'Masha', balance: 500) }
+
+  let!(:games) do
+    [
+      create(:game, user: vasya, current_level: 0, created_at: Time.parse('19.04.2022, 10:00')),
+      create(:game, user: masha, current_level: 4, created_at: Time.parse('19.04.2022, 10:00'), finished_at: Time.parse('19.04.2022, 10:10'), prize: 500)
+    ]
+  end
+
+  before { login_as vasya }
+
+  scenario 'Vasya watches Masha\'s page' do
+    visit '/'
+
+    expect(page).to have_content 'Masha'
+
+    click_link 'Masha'
+
+    expect(page).to have_content 'Billionaire'
+    expect(page).to have_content 'Vasya'
+    expect(page).to have_content '0 ₽'
+    expect(page).to have_content 'Новая игра'
+    expect(page).to have_content 'Выйти'
+
+    expect(page).to have_current_path "/users/#{masha.id}"
+    expect(page).not_to have_content 'Сменить имя и пароль'
+    expect(page).to have_content 'Masha'
+
+    expect(page).to have_content '#'
+    expect(page).to have_content 'Дата'
+    expect(page).to have_content 'Вопрос'
+    expect(page).to have_content 'Выигрыш'
+    expect(page).to have_content 'Подсказки'
+
+    expect(page).to have_content '1'
+    expect(page).to have_content 'деньги'
+    expect(page).to have_content '19 апр., 10:00'
+    expect(page).to have_content '4'
+    expect(page).to have_content '500 ₽'
+  end
+end
